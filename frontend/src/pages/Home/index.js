655cc8f70b644f9a8e7f8c38c9471f36a178f20b
Tafita Hery <tafitahery@gmail.com>
2022-04-09 11:18:38 +0300

    Ajout du formulaire dans la page Home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -4,6 +4,28 @@ const Home = () => {
   return (
     <div>
       <h1>Réservation</h1>
+      <form action="">
+        <div>
+          <label htmlFor="date_entree">Date entrée : </label>
+          <input type="date" id="date_entree" />
+        </div>
+        <div>
+          <label htmlFor="date_sortie">Date sortied : </label>
+          <input type="date" id="date_sortie" />
+        </div>
+        <div>
+          <label htmlFor="nom_client">Nom du Client : </label>
+          <select id="nom_client">
+            <option value=""> --- </option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="nom_chambre">Chambre : </label>
+          <select id="nom_chambre">
+            <option value=""> --- </option>
+          </select>
+        </div>
+      </form>
     </div>
   );
 };

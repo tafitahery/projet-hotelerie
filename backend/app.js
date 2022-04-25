bbc9cad962b751a299b045c5df786fce1ef5901d
Tafita Hery <tafitahery@gmail.com>
2022-04-25 15:02:48 +0300

    backend: implementation all routes and all controllers for Client

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const clientRoutes = require('./routes/client');
+
 const app = express();
 
 mongoose
@@ -23,5 +25,7 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+app.use('/api/client', clientRoutes);
 
 module.exports = app;
--- backend/controller/client.js
+++ backend/controller/client.js
@@ -1,11 +1,38 @@
-const Chambre = require('../models/Chambre');
+const Client = require('../models/Client');
 
-exports.createClient = (req, res, next) => {};
+exports.createClient = (req, res, next) => {
+  delete req.body._id;
 
-exports.modifyClient = (req, res, next) => {};
+  const client = new Client({
+    ...req.body,
+  });
 
-exports.deleteClient = (req, res, next) => {};
+  client
+    .save()
+    .then(() => res.status(201).json({ message: 'Client enregistré !' }))
+    .catch((error) => res.status(400).json({ error }));
+};
 
-exports.getOneClient = (req, res, next) => {};
+exports.modifyClient = (req, res, next) => {
+  Client.updateOne({ _id: req.params.id }, { ...req.body, _id: req.body.id })
+    .then(() => res.status(200).json({ message: 'Client modifié !' }))
+    .catch((error) => res.status(400).json({ error }));
+};
 
-exports.getAllClient = (req, res, next) => {};
+exports.deleteClient = (req, res, next) => {
+  Client.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Client supprimé !' }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.getOneClient = (req, res, next) => {
+  Client.findOne({ _id: req.params.id })
+    .then((client) => res.status(200).json(client))
+    .catch((error) => res.status(404).json({ error }));
+};
+
+exports.getAllClient = (req, res, next) => {
+  Client.find()
+    .then((clients) => res.status(200).json({ clients }))
+    .catch((error) => res.status(400).json({ error }));
+};

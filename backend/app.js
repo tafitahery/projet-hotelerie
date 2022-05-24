26d48036c44ca79a69045ee0a277bd12c6b5eead
Tafita Hery <tafitahery@gmail.com>
2022-05-24 20:06:17 +0300

    Create file server.js and app.js

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost:27017/hotel', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connexion à MongoDb réussie !'))
+  .catch(() => console.log('Connexion à MongoDb échouée !'));
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+
+module.exports = app;
